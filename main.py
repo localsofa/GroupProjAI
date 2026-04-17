d43e9cbe3d52bf74386a33b94cb3666127bba40a
localsofa <sophiahudelistt@gmail.com>
2026-04-17 15:44:17 +0100

    soup applied

--- main.py
+++ main.py
@@ -9,16 +9,25 @@
 # pandas! maybe analystics or visualization?
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, soup
 
-# take URL input from user eg & do things yay: 
+# take URL input from user eg & scrape it yay: 
 
 def scrape():
     
     url = input("Enter URL: ")
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup)
 
-# scrape()
-# later - take info, create subtopics, save under some sort of title if user has requested it in UI
+    # beautify it
+    title = soup.select_one('h1').text
+    text = soup.select_one('p').text
+    link = soup.select_one('a').get('href')
+    
+
+
+# scrape() if button clicked in ui
+
+
+# later - take info, create subtopics, save under some sort of title if user has requested it in UI - prob an if else
+# store in Streamlit Community Cloud? 
